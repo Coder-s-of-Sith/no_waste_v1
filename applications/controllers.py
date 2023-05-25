972435b5cc8e7927592f74a06bbf1ca596c3c9f4
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-05-25 02:43:50 +0000

    Co-authored-by: ShubhamYadav9416 <ShubhamYadav9416@users.noreply.github.com> Co-authored-by: Gagneet Kaur <gagneet-kaur@users.noreply.github.com> Co-authored-by: ProCodec <error9098x@users.noreply.github.com>

--- applications/controllers.py
+++ applications/controllers.py
@@ -14,10 +14,12 @@ def customer_login():
         input_boxes = [{"label":"Name", "type": "text", "name" : "c_name", "required" : "True"},{"label":"Email", "type": "email", "name" : "c_mail", "required" : "True"},{"label":"Password", "type": "password", "name" : "password", "required" : "True"}]
         submit_button = "Sign In"
         return render_template("layout_login.html",page=page, input_boxes = input_boxes, submit_button = submit_button)
+    if request.method == "POST":
+        request.form
 
 
 @app.route("/customer_register", methods=["GET","POST"])
-def customer_register():
+def customer_register():   
     if request.method == "GET":
         page = "customer"
         input_boxes = [{"label":"Name", "type": "text", "name" : "c_name", "required" : "True"},{"label":"Email", "type": "email", "name" : "email", "required" : "True"},{"label":"Password", "type": "password", "name" : "password", "required" : "True"}]
@@ -28,15 +30,17 @@ def customer_register():
 def vendor_login():
     if request.method == "GET":
         page = "vendor"
-        input_boxes = [{"label":"Bussiness Name", "type": "text", "name" : "v_name", "required" : "True"},{""},{"label":"Bussiness Email", "type": "email", "name" : "vmail", "required" : "True"},{"label":"Password", "type": "password", "name" : "password", "required" : "True"}]
-        submit_button = "Sign Up"
-        return render_template("layout_login.html", page = page)
+        input_boxes = [{"label":"Bussiness Email", "type": "email", "name" : "vmail", "required" : "True"},{"label":"Password", "type": "password", "name" : "password", "required" : "True"}]
+        submit_button = "Sign In"
+        return render_template("layout_login.html", page = page,input_boxes = input_boxes, submit_button = submit_button)
 
 @app.route("/vendor_register", methods=["GET","POST"])
 def vendor_register():
     if request.method == "GET":
         page = "vendor"
-        return render_template("layout_login.html",page = page)
+        input_boxes = [{"label":"Bussiness Name", "type": "text", "name" : "v_name", "required" : "True"},{"label":"Bussiness Email", "type": "email", "name" : "vmail", "required" : "True"},{"label":"Password", "type": "password", "name" : "password", "required" : "True"}]
+        submit_button = "Sign Up"
+        return render_template("layout_login.html",page = page,input_boxes=input_boxes, submit_button = submit_button)
 
 
 @app.route("/place_order",methods=["GET","POST"])
